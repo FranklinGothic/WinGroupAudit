f194ebae4470aa98e5d255513d38acc5fde103ab
Your Name <your.email@example.com>
2025-07-03 16:00:07 -0700

    print groups and subgroups - Now store the data

--- group_members.py
+++ group_members.py
@@ -1,15 +1,21 @@
 import os, subprocess
 
 class group_members:
-    def __init__(self, group):
-        self.group = group
-        self.get_members()
+    def __init__(self):
+         pass
     
-    def get_members(self):
-
-        command = fr"Get-ADGroupMember -Identity '{self.group}' -Recursive | Where-Object {{ $_.objectClass -eq 'group' }} | Select-Object -ExpandProperty Name"
+    @staticmethod
+    def get_members(group, add_distance):
+        i = 0
+        command = fr"Get-ADGroupMember -Identity '{group}' | Where-Object {{$_.objectClass -eq 'group'}} | Select-Object -ExpandProperty Name"
         response = subprocess.Popen(f"powershell.exe -Command \"& {{ {command} }}\"", 
                         stdout=subprocess.PIPE, text=True)
-        
-        output, _ = response.communicate()    # Wait for the process to finish and get the output
-        print(output.strip())
+        output, _ = response.communicate()
+        group_list = output.strip().splitlines()
+        for group_name in group_list:
+            i = 0
+            while i < add_distance:
+                print("\t", end="")
+                i+=1
+            print(f"\t - {group_name}")
+            group_members.get_members(group_name, add_distance + 1)
--- shares.py
+++ shares.py
@@ -8,8 +8,9 @@ class shares:
         self.get_shares()
     
     def get_shares(self):
-        command = r"Get-SmbShare | Where-Object {$_.Name -notmatch '\$$'} | Select-Object -ExpandProperty Name"
-        response = subprocess.Popen(f"powershell.exe -Command \"& {{ {command} }}\"", 
+        #Gets all shares - ALL NAMES
+        share_name_command = r"Get-SmbShare | Where-Object {$_.Name -notmatch '\$$'} | Select-Object -ExpandProperty Name"
+        response = subprocess.Popen(f"powershell.exe -Command \"& {{ {share_name_command} }}\"", 
                         stdout=subprocess.PIPE, text=True)
         
         output, _ = response.communicate()    # Wait for the process to finish and get the output
@@ -22,16 +23,27 @@ class shares:
         for share_name, g_list in self.shares.items():
             print(f"Who has permissions to <{share_name}>:")
             for group_name in g_list:
-                group_members(group_name)
                 print(f"\t  - {group_name}")
+                group_members.get_members(group_name, 2)
+            print("\n")
 
     def get_group_list(self, share):
-        command = fr"Get-SmbShareAccess -Name '{share}' | Select-Object -ExpandProperty AccountName"
-        response = subprocess.Popen(f"powershell.exe -Command \"& {{ {command} }}\"", 
+        #Get all groups associated with that share
+        share_group_command = fr"Get-SmbShareAccess -Name '{share}' | ForEach-Object {{ $_.AccountName -replace '^.*\\', '' }}"
+        response = subprocess.Popen(f"powershell.exe -Command \"& {{ {share_group_command} }}\"", 
                         stdout=subprocess.PIPE, text=True)
 
         output, _ = response.communicate()
         group_list = output.strip().splitlines()
-        return group_list
+        groups_to_keep = []
+        for group in group_list:
+            check_group_command = fr"Get-ADObject -Filter {{ SamAccountName -eq '{group}' }} -Properties objectClass | Select-Object -ExpandProperty ObjectClass"
+            response = subprocess.Popen(f"powershell.exe -Command \"& {{ {check_group_command} }}\"", 
+                            stdout=subprocess.PIPE, text=True)
+
+            output, _ = response.communicate()
+            if output.strip() == "group":
+                groups_to_keep.append(group)
+        return groups_to_keep
 
 shares()
